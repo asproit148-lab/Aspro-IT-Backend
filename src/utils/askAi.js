2d48b3f4687a10cc6f36bb2a1bfbdc75aec126b7
abhisheko5 <86157818+abhisheko5@users.noreply.github.com>
2025-12-05 13:42:06 +0530

    fixed bot

--- src/utils/askAi.js
+++ src/utils/askAi.js
@@ -23,7 +23,7 @@ export const askAI = async (prompt) => {
       console.log("➡️ process.cwd():", process.cwd());
 console.log("➡️ __dirname:", __dirname);
 
-const filePath = path.resolve(__dirname, "scrapedData.txt");
+const filePath = path.resolve( "scrapedData.txt");
 console.log("Looking for file at:", filePath);
 
 context = fs.readFileSync(filePath, "utf-8");
